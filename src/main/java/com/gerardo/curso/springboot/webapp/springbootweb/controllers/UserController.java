787b64ba1896f91a4078b2d6f1f2f320c0561456
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-04-28 16:48:34 -0300

    thymeleaf

--- src/main/java/com/gerardo/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/gerardo/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,13 +1,21 @@
 package com.gerardo.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title",
+                "Hello World Spring boot MVC");
+
+        model.addAttribute("name",
+                "Gerardo");
+        model.addAttribute("lastname",
+                "Lopez");
         return "details";
     }
 }
